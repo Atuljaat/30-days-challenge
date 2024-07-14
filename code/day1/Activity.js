2b33dcefd416bff1ae71a194e938101feb22aa19
Atuljaat <147144352+Atuljaat@users.noreply.github.com>
2024-07-14 12:33:35 +0530

    Day 1 done

---
+++ code/day1/Activity.js
@@ -0,0 +1,51 @@
+// Activity 1 :  Variable Declaration
+// Task 1
+var num = 1
+// console.log(num)
+
+//Task 2
+let drink = 'chai'
+// console.log(drink)
+
+
+// Activity 2 : Constant Declaration
+//Task 3
+const ans = true
+// console.log(ans)
+
+//Activity 3 : Data Types
+// Task 4
+let letter = 'chai'
+let digit = 12
+let answer = true
+let nothing = null
+let idk = undefined
+let object = {name:'tea'}
+let myarray = [1,2,3,4]
+// console.log(`Type of ${letter} is ${typeof(letter)}`)
+// console.log(`Type of ${digit} is ${typeof(digit)}`)
+// console.log(`Type of ${answer} is ${typeof(answer)}`)
+// console.log(`Type of ${nothing} is ${typeof(nothing)}`)
+// console.log(`Type of ${idk} is ${typeof(idk)}`)
+// console.log(`Type of object is ${typeof(object)}`)
+// console.log(`Type of myarray is ${typeof(myarray)}`)
+
+// Array and null type is an Object
+
+
+// Activity 4 : Reassigning Variables
+// Task 5 
+let firstName = 'Atul'
+console.log(`Before reassigning the value of firstname is ${firstName}`)
+ firstName = 'Rahul'
+console.log(`After reassiging the value of firstname is ${firstName} `)
+
+// Activity 5 : Understanding const
+// Task 6
+const Naam = 'Bhoot'
+Naam = 'hoo'
+// Error : TypeError: Assignment to constant variable.
+// You cannot assign value twice to a const
+
+
+
